66c59119013e81f2a9f31e9eae89e25c3e1898a4
Monga94 <monga94@gmail.com>
2018-10-25 09:45:18 +0200

    compiler

--- a.b-DataPath.core/Datapath_Reg_generic.vhd
+++ a.b-DataPath.core/Datapath_Reg_generic.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.all;
 entity D_Reg_generic is
 	Generic (N: integer := 8);
 	Port (	D:		In	std_logic_vector(N-1 downto 0);
-			CLK:		In	std_logic;
+			CLK:	In	std_logic;
 			RESET:	In	std_logic;
 			ENABLE:	In	std_logic;
 			Q:		Out	std_logic_vector(N-1 downto 0));
@@ -16,13 +16,13 @@ begin
 	
 	PASYNCH: process(CLK,RESET)
 	begin
-	  if RESET='0' then
-	    Q <= (others => '0');
-	  elsif CLK'event and CLK='1' then -- positive edge triggered:
-	    if ENABLE='1' then
-		  Q <= D;
+		if RESET='0' then
+			Q <= (others => '0');
+		elsif CLK'event and CLK='1' then -- positive edge triggered:
+			if ENABLE='1' then
+				Q <= D;
+			end if;
 		end if;
-	  end if;
 	end process;
 
 end ASYNC;
