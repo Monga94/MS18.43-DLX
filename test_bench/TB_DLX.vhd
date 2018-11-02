a429cdf2765331fe53ecdfbcc1379170278eeeea
Monga94 <monga94@gmail.com>
2018-11-02 16:43:21 +0100

    Update test_bench/TB_DLX.vhd

--- test_bench/TB_DLX.vhd
+++ test_bench/TB_DLX.vhd
@@ -7,38 +7,25 @@ entity tb_dlx is
 end tb_dlx;
 
 architecture TEST of tb_dlx is
-
-
-    constant SIZE_IR      : integer := 32;       -- Instruction Register Size
-    constant SIZE_PC      : integer := 32;       -- Program Counter Size
-    constant SIZE_ALU_OPC : integer := 6;        -- ALU Op Code Word Size in case explicit coding is used
     signal Clock: std_logic := '0';
     signal Reset: std_logic := '1';
 
-    component DLX
-       generic (
-       IR_SIZE      : integer := 32;       -- Instruction Register Size
-       PC_SIZE      : integer := 32       -- Program Counter Size
-       );  	   -- ALU_OPC_SIZE is explicit ALU Op Code Word Size
-       port (
-       Clk : in std_logic;
-       Rst : in std_logic);                -- Active Low
-    end component;
+	component DLX
+		port(	CLK:	in std_logic;
+				RST:	in std_logic);
+	end component;
+
 begin
-
-
         -- instance of DLX
-	U1: DLX
-        Generic Map (SIZE_IR, SIZE_PC) -- SIZE_ALU_OPC)   
-	Port Map (Clock, Reset);
+	U1: DLX  
+		port map (Clock, Reset);
 	
-
-        PCLOCK : process(Clock)
+	PCLOCK : process(Clock)
 	begin
 		Clock <= not(Clock) after 0.5 ns;	
 	end process;
 	
-	Reset <= '0', '1' after 6 ns, '0' after 11 ns, '1' after 15 ns;
+	Reset <= '0' after 11 ns, '1' after 15 ns;		--'0', '1' after 6 ns, 
        
 
 end TEST;
